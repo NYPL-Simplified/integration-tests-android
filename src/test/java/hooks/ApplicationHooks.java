4e778d25ae2cef145ca40868cac1ed19d928cb01
e2.suboch <e2.suboch@a1qa.com>
2020-08-21 17:54:08 +0300

    Added appium, cucumber, browser stack support

---
+++ src/test/java/hooks/ApplicationHooks.java
@@ -0,0 +1,13 @@
+package hooks;
+
+import aquality.appium.mobile.application.AqualityServices;
+import io.cucumber.java.After;
+
+public class ApplicationHooks {
+    @After(order = 0)
+    public void closeApplication() {
+        if (AqualityServices.isApplicationStarted()) {
+            AqualityServices.getApplication().quit();
+        }
+    }
+}
---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,22 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = {"src/test/java/features"},
+        glue = {
+                "hooks",
+                "stepdefinitions"
+        },
+        strict = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider()
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
