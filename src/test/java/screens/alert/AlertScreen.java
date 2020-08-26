5bfe54ab030625a797a79434793fc0f1f8a68cb4
e2.suboch <e2.suboch@a1qa.com>
2020-08-26 14:58:13 +0300

    Added scenario Remove library

---
+++ src/test/java/screens/alert/AlertScreen.java
@@ -0,0 +1,12 @@
+package screens.alert;
+
+import aquality.appium.mobile.screens.Screen;
+import org.openqa.selenium.By;
+
+public abstract class AlertScreen extends Screen {
+    protected AlertScreen(By locator) {
+        super(locator, "Alert");
+    }
+
+    public abstract void accept();
+}
