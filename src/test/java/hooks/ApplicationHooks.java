f46a631f16151165f802b640537d897a11efda3d
e2.suboch <e2.suboch@a1qa.com>
2020-10-01 21:18:53 +0300

    Updated after-steps order, added logging messages, updated workflow

--- src/test/java/hooks/ApplicationHooks.java
+++ src/test/java/hooks/ApplicationHooks.java
@@ -6,6 +6,7 @@ import io.cucumber.java.After;
 public class ApplicationHooks {
     @After(order = 0)
     public void closeApplication() {
+        AqualityServices.getLogger().info("Closing application");
         if (AqualityServices.isApplicationStarted()) {
             AqualityServices.getApplication().quit();
         }
--- src/test/java/hooks/logout/LogoutHooks.java
+++ src/test/java/hooks/logout/LogoutHooks.java
@@ -6,7 +6,6 @@ import hooks.logout.components.AbstractLogoutHooks;
 import hooks.logout.components.ILogoutHooks;
 import io.cucumber.java.After;
 import stepdefinitions.BaseSteps;
-import stepdefinitions.application.components.AbstractApplicationSteps;
 
 public class LogoutHooks extends BaseSteps implements ILogoutHooks {
 
@@ -18,19 +17,19 @@ public class LogoutHooks extends BaseSteps implements ILogoutHooks {
     }
 
     @Override
-    @After(value = "@logout", order = 1)
+    @After(value = "@logout", order = 2)
     public void closeApplication() {
         abstractLogoutHooks.closeApplication();
     }
 
     @Override
-    @After(value = "@cancelHold", order = 2)
+    @After(value = "@cancelHold", order = 3)
     public void cancelHold() {
         abstractLogoutHooks.cancelHold();
     }
 
     @Override
-    @After(value = "@cancelGet", order = 2)
+    @After(value = "@cancelGet", order = 3)
     public void cancelGet() {
         abstractLogoutHooks.cancelGet();
     }
