54a10a18d16917cb9b25362a56018078bfb450be
e2.suboch <e2.suboch@a1qa.com>
2020-10-06 13:00:01 +0300

    Updated logout logic for ios

--- src/test/java/hooks/logout/components/impl/IosLogoutHooks.java
+++ src/test/java/hooks/logout/components/impl/IosLogoutHooks.java
@@ -4,7 +4,6 @@ import aquality.appium.mobile.application.PlatformName;
 import factories.steps.StepsType;
 import framework.utilities.ScenarioContext;
 import hooks.logout.components.AbstractLogoutHooks;
-import io.cucumber.java.After;
 import screens.bottommenu.BottomMenu;
 
 @StepsType(platform = PlatformName.IOS)
@@ -20,6 +19,7 @@ public class IosLogoutHooks extends AbstractLogoutHooks {
         if (!accountScreen.state().isDisplayed()) {
             bottomMenuForm.open(BottomMenu.SETTINGS);
             settingsScreen.openAccounts();
+            accountsScreen.openFirstLibrary();
         }
         accountScreen.logOut();
         if (!accountScreen.isLogoutSuccessful()) {
