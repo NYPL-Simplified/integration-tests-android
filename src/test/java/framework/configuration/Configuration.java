e3fea552d5fadb33cb6bb3847e9cab104ae032a9
e2.suboch <e2.suboch@a1qa.com>
2020-10-08 13:34:53 +0300

    Debugging changes

--- src/test/java/framework/configuration/Configuration.java
+++ src/test/java/framework/configuration/Configuration.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.RandomUtils;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.stream.Stream;
 
 public class Configuration {
@@ -13,12 +14,12 @@ public class Configuration {
     }
 
     public static Credentials getCredentials(String libraryName) {
-        Map<String, String> systemEnvironments = System.getenv();
+        Properties systemEnvironments = System.getProperties();
         Map<String, Object> listOfCredentials = new HashMap<>();
         String variableNameBeginning = "credentials." + libraryName + ".";
-        if (systemEnvironments.keySet().stream().anyMatch(x -> x.startsWith(variableNameBeginning))) {
-            Stream<String> foundVariables = systemEnvironments.keySet().stream().filter(x -> x.startsWith(variableNameBeginning));
-            foundVariables.forEach(x -> listOfCredentials.put(x.replace(variableNameBeginning, ""), systemEnvironments.get(x)));
+        if (systemEnvironments.keySet().stream().anyMatch(x -> ((String)x).startsWith(variableNameBeginning))) {
+            Stream<Object> foundVariables = systemEnvironments.keySet().stream().filter(x -> ((String)x).startsWith(variableNameBeginning));
+            foundVariables.forEach(x -> listOfCredentials.put(((String)x).replace(variableNameBeginning, ""), systemEnvironments.get(x)));
         } else {
             listOfCredentials.putAll(Environment.getEnvironment().getMap("/credentials/" + libraryName));
         }
