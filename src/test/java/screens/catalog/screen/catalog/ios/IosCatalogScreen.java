dec029d16ecac03ee6cfbcf420cde62de0011073
hsuboch <49855539+hsuboch@users.noreply.github.com>
2020-12-21 20:30:19 +0300

    Updated books name checking logic (#149)
    Co-authored-by: e2.suboch <e2.suboch@a1qa.com>

--- src/test/java/screens/catalog/screen/catalog/ios/IosCatalogScreen.java
+++ src/test/java/screens/catalog/screen/catalog/ios/IosCatalogScreen.java
@@ -29,25 +29,25 @@ public class IosCatalogScreen extends CatalogScreen {
     private static final String LANE_BY_NAME_LOCATOR_PART = "(//XCUIElementTypeOther[.//XCUIElementTypeButton[@name=\"%1$s\"]]"
             + "/following-sibling::XCUIElementTypeCell)[1]";
     private static final String BOOK_COVER_IN_THE_LANE_LOCATOR = "/XCUIElementTypeButton";
-    private static final String FEED_LANE_TITLES_LOC = "//XCUIElementTypeOther[./following-sibling::XCUIElementTypeCell[1]]"
-            + "//XCUIElementTypeButton[1]";
+    private static final String FEED_LANE_TITLES_LOCATOR =
+            "//XCUIElementTypeOther[./following-sibling::XCUIElementTypeCell[1]]//XCUIElementTypeButton[1]";
     private static final String LIBRARY_BUTTON_LOCATOR_PATTERN =
             "//XCUIElementTypeButton[@name=\"%1$s\"]";
 
     private static final String BOOKS_LOCATOR = "//XCUIElementTypeTable//XCUIElementTypeCell//XCUIElementTypeButton[@name]";
 
-    private final ILabel firstLaneName = getElementFactory().getLabel(
-            By.xpath(FEED_LANE_TITLES_LOC), "First lane name");
+    private final ILabel firstLaneName =
+            getElementFactory().getLabel(By.xpath(FEED_LANE_TITLES_LOCATOR), "First lane name", ElementState.EXISTS_IN_ANY_STATE);
     private final ILabel categoryScreen = getElementFactory().getLabel(By.xpath("//XCUIElementTypeTable"), "Category Screen");
 
     public IosCatalogScreen() {
-        super(By.xpath(FEED_LANE_TITLES_LOC));
+        super(By.xpath(FEED_LANE_TITLES_LOCATOR));
     }
 
     @Override
     public List<String> getListOfBooksNames() {
-        List<String> listOfNames = getValuesFromListOfLabels(String.format(LANE_BY_NAME_LOCATOR_PART,
-                firstLaneName.getText()) + BOOK_COVER_IN_THE_LANE_LOCATOR);
+        state().waitForDisplayed();
+        List<String> listOfNames = getValuesFromListOfLabels(BOOKS_LOCATOR);
         AqualityServices.getLogger().info("Found list of books - " + listOfNames.stream().map(Object::toString)
                 .collect(Collectors.joining(", ")));
         return listOfNames;
@@ -56,7 +56,7 @@ public class IosCatalogScreen extends CatalogScreen {
     @Override
     public boolean isCategoryPageLoad() {
         return AqualityServices.getConditionalWait().waitFor(() ->
-                        getElementFactory().findElements(By.xpath(FEED_LANE_TITLES_LOC), ElementType.LABEL).size() > 0,
+                        getElementFactory().findElements(By.xpath(FEED_LANE_TITLES_LOCATOR), ElementType.LABEL).size() > 0,
                 Duration.ofMillis(CategoriesTimeouts.TIMEOUT_WAIT_UNTIL_CATEGORY_PAGE_LOAD.getTimeoutMillis()));
     }
 
