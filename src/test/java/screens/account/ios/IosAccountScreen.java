9f6b0d6c090aee4c253c5fdc560d05692997779b
e2.suboch <e2.suboch@a1qa.com>
2021-02-24 15:26:46 +0300

    Updated login logic for ios, removed step copy

--- src/test/java/screens/account/ios/IosAccountScreen.java
+++ src/test/java/screens/account/ios/IosAccountScreen.java
@@ -39,7 +39,8 @@ public class IosAccountScreen extends AccountScreen {
 
     @Override
     public void enterCredentials(Credentials credentials) {
-        if (!btnLogout.state().waitForDisplayed()) {
+        AqualityServices.getConditionalWait().waitFor(() -> btnLogin.state().isDisplayed() || btnLogout.state().isDisplayed());
+        if (!btnLogout.state().isDisplayed()) {
             txbCard.click();
             txbCard.clearAndType(credentials.getBarcode());
             txbPin.clearAndTypeSecret(credentials.getPin());
