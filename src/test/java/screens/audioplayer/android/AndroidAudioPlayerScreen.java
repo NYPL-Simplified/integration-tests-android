bb4d8423fa7212a691b7f2683ca41959bf14afc2
e2.suboch <e2.suboch@a1qa.com>
2021-03-15 15:30:27 +0300

    Updated locators for Android audioplayer screen

--- src/test/java/screens/audioplayer/android/AndroidAudioPlayerScreen.java
+++ src/test/java/screens/audioplayer/android/AndroidAudioPlayerScreen.java
@@ -7,6 +7,7 @@ import aquality.appium.mobile.elements.ElementType;
 import aquality.appium.mobile.elements.interfaces.IButton;
 import aquality.appium.mobile.elements.interfaces.ILabel;
 import aquality.appium.mobile.screens.screenfactory.ScreenType;
+import aquality.selenium.core.elements.ElementState;
 import constants.application.timeouts.AudioBookTimeouts;
 import constants.localization.application.catalog.TimerKeys;
 import framework.utilities.DateUtils;
@@ -24,7 +25,7 @@ public class AndroidAudioPlayerScreen extends AudioPlayerScreen {
     private static final String LOADING_SCREEN_XPATH = "//*[contains(@resource-id,\"player_toc_item_downloading_progress\")]";
     private static final String SPEED_OPTION_XPATH_LOCATOR_PATTERN = "//*[contains(@resource-id, \"player_menu_playback_rate_text\") and @text=\"%sx\"]";
     private static final String TIMER_XPATH_PATTERN_LOCATOR = "//*[contains(@resource-id, \"player_sleep_item_view_name\") and @text=\"%s\"]";
-    private static final String TIMER_SETTING_XPATH_LOCATOR_PATTERN = "//android.widget.RelativeLayout[contains(@resource-id, \"player_menu_sleep\") and @content-desc=\"Set Your Sleep Timer. The Sleep Timer Is Currently Set To Sleep At %s\"]";
+    private static final String TIMER_SETTING_XPATH_LOCATOR_PATTERN = "//*[contains(@resource-id, \"player_menu_sleep\") and @content-desc=\"Set Your Sleep Timer. The Sleep Timer Is Currently Set To Sleep At %s\"]";
 
     private final IButton btnMenu = getElementFactory().getButton(By.id("player_menu_toc"), "Menu");
     private final ILabel lblCurrentChapter = getElementFactory().getLabel(By.id("player_spine_element"), "Current chapter");
@@ -150,7 +151,7 @@ public class AndroidAudioPlayerScreen extends AudioPlayerScreen {
     @Override
     public boolean isTimerSetTo(TimerKeys timerSetting) {
         String timerSettingName = timerSetting.i18n();
-        return getElementFactory().getButton(By.xpath(String.format(TIMER_SETTING_XPATH_LOCATOR_PATTERN, timerSettingName)), timerSettingName).state().waitForDisplayed();
+        return getElementFactory().getButton(By.xpath(String.format(TIMER_SETTING_XPATH_LOCATOR_PATTERN, timerSettingName)), timerSettingName, ElementState.EXISTS_IN_ANY_STATE).state().waitForDisplayed();
     }
 
     @Override
